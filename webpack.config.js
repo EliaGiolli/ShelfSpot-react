676ceead167fe54d0ab29be9cfe39231d0fdfbc0
Elia Giolli <146369060+EliaGiolli@users.noreply.github.com>
2024-02-03 15:47:59 +0100

    cancellata cartella dist in favore della cartella docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "js/[name].[contenthash].js",
     assetModuleFilename: "img/[hash][ext][query]",
   },
